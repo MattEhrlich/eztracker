842e79563263a40541c6c405534e32f9461ca01b
MattEhrlich <mehrlichdev@gmail.com>
2017-03-27 14:44:14 -0400

    home page pre-lim design

---
+++ app/views/staticpages/sales.html.erb
@@ -0,0 +1,1 @@
+<h1>Sales Page</h1>
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   resources :workouts, only: [:index]
   resources :exercises, only: []
   resources :sessions, only: [:update, :show, :create]
+  
+  get 'product', to: 'staticpages#product'
+  get 'app', to: 'staticpages#app'
+  get 'about', to: 'staticpages#about'
+  get 'sales', to: 'staticpages#sales'
+
 end
