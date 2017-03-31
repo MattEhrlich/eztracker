7e8aca94b4b128121e9b8b2806bfb47cf5f8f657
MattEhrlich <mehrlichdev@gmail.com>
2017-03-31 15:01:18 -0400

    dashboard created and redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+ 
+  def after_sign_up_path_for(resource)
+    redirect_to dashboard_path(current_user)
+  end
+  
+  
   private
 
 end
