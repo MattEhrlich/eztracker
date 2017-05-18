fa81064b23ca87d2049c8028d7115d3d0ba550a0
MattEhrlich <mehrlichdev@gmail.com>
2017-05-18 16:40:17 -0400

    migrating db

--- db/migrate/20170518195502_change_motions_to_arrays.rb
+++
@@ -1,7 +0,0 @@
-class ChangeMotionsToArrays < ActiveRecord::Migration[5.0]
-  def change
-	 change_column :ibeacons, :x_motion, :string
-	 change_column :ibeacons, :y_motion, :string
-	 change_column :ibeacons, :z_motion, :string
-  end
-end
