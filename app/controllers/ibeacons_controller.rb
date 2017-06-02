b6742a4f2c325c84e6cc1de2ee79f2eee45ef0c4
Patrick Hagmann <phagmann1@gmail.com>
2017-06-02 17:59:00 +0000

    polynomial heroku gem test

--- app/controllers/ibeacons_controller.rb
+++ app/controllers/ibeacons_controller.rb
@@ -2,6 +2,15 @@ class IbeaconsController < ApplicationController
 	skip_before_filter  :verify_authenticity_token
 	require './lib/algorithm.rb'
 	def index 
+
+		@info = Ibeacon.new 
+		@info.x_motion = "-453.0,-140.0,-890.0,-484.0,-578.0,-437.0,-1234.0,-406.0,-406.0,-453.0,-515.0,-406.0,-531.0,-390.0,-625.0,-109.0,-703.0,-500.0,-375.0,31.0,"
+ 		@info.y_motion = "-812.0,-671.0,906.0,484.0,1000.0,750.0,1000.0,234.0,906.0,718.0,859.0,671.0,812.0,609.0,1078.0,859.0,968.0,750.0,390.0,-31.0,"
+ 		@info.z_motion = "687.0,953.0,-531.0,-312.0,-203.0,-156.0,312.0,-281.0,-125.0,-265.0,78.0,-46.0,-203.0,-140.0,-156.0,-187.0,-156.0,-406.0,640.0,1031.0,"
+ 		@info.exercise_name = @info.classify_exercise(@info.x_motion,@info.y_motion,@info.z_motion)
+ 		@info.reps_counted = @info.rep_count(@info.x_motion,@info.y_motion,@info.z_motion)
+ 		@info.save
+ 	
 		@data = Ibeacon.all
 	end
 	
