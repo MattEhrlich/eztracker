58c1c304eec3cf44ddbeee82837ec578e8fa0d24
Patrick Hagmann <phagmann1@gmail.com>
2017-07-27 20:10:40 +0000

    attempt 5 heroku

--- app/controllers/ibeacons_controller.rb
+++ app/controllers/ibeacons_controller.rb
@@ -44,8 +44,10 @@ class IbeaconsController < ApplicationController
             @info.reps_counted = Rep.rep_predict(beacon_params["x_motion"],beacon_params["y_motion"],beacon_params["z_motion"], @info.exercise_name)
             @info.weight_lb = beacon_params["weight_lb"][9..-2].to_i
             @info.save
-            respond_to do |format|
-               format.js {render inline: "location.reload();" }
+            if @info.save
+               respond_to do |format|
+                  format.js {render inline: "location.reload();" }
+               end
             end
          end
       end   
