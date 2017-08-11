28de5bdf4bc71b2be50e6d3295aaea8d0cd48ebd
Patrick Hagmann <phagmann1@gmail.com>
2017-08-11 21:44:01 +0000

    updates screen one at a time

--- app/controllers/ibeacons_controller.rb
+++ app/controllers/ibeacons_controller.rb
@@ -4,16 +4,16 @@ class IbeaconsController < ApplicationController
 	require './lib/rep.rb'
    require './lib/rotate.rb'
 	def index 
-		@total_exercises = Ibeacon.count  
-		@total_reps = Ibeacon.all.sum(:reps_counted)
-		@total_weight = Ibeacon.all.sum(:weight_lb)   
-      p "hello"
+ 
       respond_to do |format|
       format.html {
+        @total_exercises = Ibeacon.count  
+        @total_reps = Ibeacon.all.sum(:reps_counted)
+        @total_weight = Ibeacon.all.sum(:weight_lb)  
         @data = Ibeacon.all
       }
       format.json { 
-        render json: Ibeacon.all
+        render json: [Ibeacon.last,Ibeacon.count]
       }
     end
 	end
@@ -21,22 +21,22 @@ class IbeaconsController < ApplicationController
 	def new
 	end
 	
-# 	def create
-#       x = Algorithm.array_string_to_array("765.0,906.0,296.0,328.0,265.0,0.0,812.0,984.0,765.0,968.0,1015.0,1203.0,-125.0,843.0,781.0,671.0,")
-#       y = Algorithm.array_string_to_array("-1156.0,-1093.0,-234.0,-250.0,-156.0,46.0,-750.0,-859.0,-796.0,-828.0,-828.0,-984.0,218.0,-703.0,-546.0,-718.0,")
-#       z = Algorithm.array_string_to_array("-218.0,-46.0,31.0,109.0,109.0,171.0,187.0,250.0,156.0,93.0,203.0,46.0,218.0,-234.0,-343.0,-281.0,")
-#       p "check"
-#       if x.length >= 6
-#       		@info = Ibeacon.new 
-#       		@info.x_motion = "765.0,906.0,296.0,328.0,265.0,0.0,812.0,984.0,765.0,968.0,1015.0,1203.0,-125.0,843.0,781.0,671.0,"
-#        		@info.y_motion = "-1156.0,-1093.0,-234.0,-250.0,-156.0,46.0,-750.0,-859.0,-796.0,-828.0,-828.0,-984.0,218.0,-703.0,-546.0,-718.0,"
-#        		@info.z_motion = "-218.0,-46.0,31.0,109.0,109.0,171.0,187.0,250.0,156.0,93.0,203.0,46.0,218.0,-234.0,-343.0,-281.0,"
-#        		@info.exercise_name = @info.classify_exercise(@info.x_motion,@info.y_motion,@info.z_motion)
-#        		@info.weight_lb = 20
-#             @info.reps_counted = Rep.rep_predict(@info.x_motion,@info.y_motion,@info.z_motion, @info.exercise_name)
-#        		@info.save
-#       end
-# end
+	def create
+      x = Algorithm.array_string_to_array("765.0,906.0,296.0,328.0,265.0,0.0,812.0,984.0,765.0,968.0,1015.0,1203.0,-125.0,843.0,781.0,671.0,")
+      y = Algorithm.array_string_to_array("-1156.0,-1093.0,-234.0,-250.0,-156.0,46.0,-750.0,-859.0,-796.0,-828.0,-828.0,-984.0,218.0,-703.0,-546.0,-718.0,")
+      z = Algorithm.array_string_to_array("-218.0,-46.0,31.0,109.0,109.0,171.0,187.0,250.0,156.0,93.0,203.0,46.0,218.0,-234.0,-343.0,-281.0,")
+      p "check"
+      if x.length >= 6
+      		@info = Ibeacon.new 
+      		@info.x_motion = "765.0,906.0,296.0,328.0,265.0,0.0,812.0,984.0,765.0,968.0,1015.0,1203.0,-125.0,843.0,781.0,671.0,"
+       		@info.y_motion = "-1156.0,-1093.0,-234.0,-250.0,-156.0,46.0,-750.0,-859.0,-796.0,-828.0,-828.0,-984.0,218.0,-703.0,-546.0,-718.0,"
+       		@info.z_motion = "-218.0,-46.0,31.0,109.0,109.0,171.0,187.0,250.0,156.0,93.0,203.0,46.0,218.0,-234.0,-343.0,-281.0,"
+       		@info.exercise_name = @info.classify_exercise(@info.x_motion,@info.y_motion,@info.z_motion)
+       		@info.weight_lb = 20
+            @info.reps_counted = Rep.rep_predict(@info.x_motion,@info.y_motion,@info.z_motion, @info.exercise_name)
+       		@info.save
+      end
+end
 
    def create
       x = Algorithm.array_string_to_array(beacon_params["x_motion"])
