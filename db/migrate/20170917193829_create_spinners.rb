d7d57060ad44b43d6c687f33b91b81324ef0486f
Patrick Hagmann <phagmann1@gmail.com>
2017-09-17 20:49:08 +0000

    fallback

--- db/migrate/20170917193829_create_spinners.rb
+++
@@ -1,8 +0,0 @@
-class CreateSpinners < ActiveRecord::Migration[5.0]
-  def change
-    create_table :spinners do |t|
-      t.integer :is_moving
-      t.timestamps
-    end
-  end
-end
--- spec/factories/spinners.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :spinner do
-    
-  end
-end
