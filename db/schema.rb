c55f297c3584bf322a47bb443f82b4c0b51b2850
Patrick Hagmann <phagmann1@gmail.com>
2017-09-17 20:20:22 +0000

    new s

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405212851) do
+ActiveRecord::Schema.define(version: 20170917193829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20170405212851) do
     t.integer  "weight_lb"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "spinners", force: :cascade do |t|
+    t.integer  "is_moving"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "userprofiles", force: :cascade do |t|
